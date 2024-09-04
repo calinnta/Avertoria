1a994845a954ae952c5fbedb32e13a4d4e01eb07
calinnta <nitzahh1981@gmail.com>
2024-09-04 14:21:40 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -14,6 +14,12 @@
 
     };
 
+    function getCurrentYear() {
+        var d = new Date();
+        var year = d.getFullYear();
+        document.querySelector("#displayDateYear").innerText = year;
+      }
+      getCurrentYear()
 
    /* preloader
     * -------------------------------------------------- */
